7e7b601cee79ca8e1f0390d5cd1ea57fc81042db
tlans21 <cjstlans21@naver.com>
2024-02-05 17:40:16 +0900

    DataFrame to csv function, timer synchroinize

--- test.py
+++ test.py
@@ -7,16 +7,12 @@ from PyQt5.QtCore import Qt, QUrl, QObject, QTimer, QThread, pyqtSignal,  QSetti
 from PyQt5.QtGui import QColor
 from PyQt5.QtGui import QFont
 from datetime import datetime
-
 import time
 import os
 import serial
 import threading
 import time
 import csv
-
-
- 
 
 if getattr(sys, 'frozen', False):
     # Running in PyInstaller bundle
@@ -64,7 +60,7 @@ class Thread1(QThread):
             # 아래의 값은 serial의 스레드가 데이터를 읽는 시간 * 9 는 되어야함
             time.sleep(0.15)
             progress_percentage = int((step + 1) / total_steps * 100)
-            self.progress_signal.emit(progress_percentage)
+            self.progress_signal.emit(progress_percentage)   
         self.record_signal.emit(self.time_record)
         self.close_signal.emit(True)
             
@@ -75,6 +71,7 @@ class WindowClass(QMainWindow, form_class):
     progress_signal = pyqtSignal(int)
     close_signal = pyqtSignal(bool)
     record_signal = pyqtSignal(list)
+    
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -88,6 +85,7 @@ class WindowClass(QMainWindow, form_class):
         self.received_data = []
         self.close_signal.connect(self.updateCloseValue)
         self.record_signal.connect(self.updateRecordTime)
+        
         # 시스템 테이블 column 헤더와 row 헤더 안보이도록 설정
         self.SystemTable.horizontalHeader().setVisible(False)
         self.SystemTable.verticalHeader().setVisible(False)
@@ -138,7 +136,9 @@ class WindowClass(QMainWindow, form_class):
         self.recordStart_Btn.setEnabled(False)
         self.recordStart_Btn.clicked.connect(self.recordStart_btn_push)
         self.time_record = []
-
+        self.dataTable_record = []
+        self.systemTable_record = []
+        self.dianosticTable_record = []
         # record Finish 버튼
         self.recordFinish_Btn.clicked.connect(self.recordFinish_btn_push)
         # record Finish 버튼 디폴트값 False
@@ -185,13 +185,38 @@ class WindowClass(QMainWindow, form_class):
 
     def updateRecordTime(self, record):
         self.time_record = record
+        print('타임레코드 : ', self.time_record)
+        recordDataLegnth = len(record)
+        self.recordDataLengthLabel.setText(f'Record data count : {recordDataLegnth}')
+        ## 데이터가 흐르기 시작한 경우
+        if self.record_flag == True:
+            # 데이터 프레임 테이블 컨버트후 저장
+            convertedTable_df = self.convertTableToList(self.df_table)
+            self.dataTable_record.append(convertedTable_df)
+            # 시스템 테이블 컨버트후 저장
+            convertedTable_system = self.convertTableToList(self.SystemTable)
+            self.systemTable_record.append(convertedTable_system)
+            # 다이아노스틱 테이블 컨버트후 저장
+            convertedTable_dianostic = self.convertTableToList(self.dianostic_table)
+            self.dianosticTable_record.append(convertedTable_dianostic)
 
     def updateInputValue(self, value):
         self.input_Value = value
 
     def updateDataCheckPoint(self, datacheckPoint):
         self.dataCheckPoint = datacheckPoint
+
+    def updateRemainingTime(self):
+        remainingTime = self.simulate_timer.remainingTime()
+        if remainingTime >=500:
+            self.remainingTimeLabel.setText(f'Time remaining until next cycle : {round(self.simulate_timer.remainingTime() / 1000)}')
+            print(remainingTime)
+        else:
+            print(remainingTime)
+            self.countDownTimer.stop()
+            self.remainingTimeLabel.setText("Countdown finished")
         
+
     def updateCloseValue(self, value):
         # 데이터가 흐르는 것을 마쳤을 때, True 아니면 False
         if value == True:
@@ -207,7 +232,7 @@ class WindowClass(QMainWindow, form_class):
             # 데이터가 흐르는것을 마쳤을때 + 데이터 기록 start 버튼 누른 경우
             elif self.record_flag == True:
                 self.recordStart_Btn.setEnabled(False)
-
+                self.recordFinish_Btn.setEnabled(True)
         elif value == False:
             # 흐르는 동안에는 포트 닫기 버튼 비활성화
             self.closeBtn.setEnabled(value)
@@ -216,6 +241,7 @@ class WindowClass(QMainWindow, form_class):
             self.Interval_Cancel_Btn_2.setEnabled(value)
             # record start 버튼 비활성화
             self.recordStart_Btn.setEnabled(value)
+            self.recordFinish_Btn.setEnabled(value)
     def openPort(self):
         # 시리얼 통신을 위한 콤보박스 지정하는 것을 저장
         self.inputBtn.setEnabled(True)
@@ -247,7 +273,7 @@ class WindowClass(QMainWindow, form_class):
                         res = self.ser.read(56)
 
                         hex_representation = ' '.join([format(byte, '02X') for byte in res])
-                        print(hex_representation)
+                        # print(hex_representation)
                         buffer = hex_representation.split()
                         for i in range(len(buffer)):
                             self.received_data.append(buffer[i])
@@ -267,7 +293,7 @@ class WindowClass(QMainWindow, form_class):
 
                             for i in range(len(buffer)):
                                 self.received_data.append(buffer[i])
-                            print(len(self.received_data))
+                            #print(len(self.received_data))
                             self.updateTable()
                         
                     except serial.SerialException as e:
@@ -303,7 +329,7 @@ class WindowClass(QMainWindow, form_class):
                     print("Stopping simulate_timer2.")
                     self.simulate_timer2.stop()    
             print("Serial port closed.")
-            print(self.ser.is_open)
+            #print(self.ser.is_open)
             self.openBtn.setEnabled(True)
             # 포트를 닫았으므로 입력 버튼 비활성화
             self.inputBtn.setEnabled(False)
@@ -339,9 +365,11 @@ class WindowClass(QMainWindow, form_class):
     def startPeriodicTask(self):
 
          # 이전 스레드가 실행 중인 경우 종료합니다.
-        if hasattr(self, 'periodicTaksThread') and self.periodicTaksThread is not None:
+        if hasattr(self, 'periodicTaskThread') and self.periodicTaskThread is not None:
             self.periodicTaskThread.terminate()
             self.periodicTaskThread.wait()  # 스레드가 종료될 때까지 기다립니다.
+
+
 
         self.period_pbar_value = 0
         self.period_pbar.setValue(self.period_pbar_value)
@@ -355,21 +383,33 @@ class WindowClass(QMainWindow, form_class):
         for row in range(0, self.dianostic_table.rowCount()):
             self.dianostic_table.takeItem(row, 1)
         self.text_list = ['A', 'B', 'C', 'D', 'E']
-        print('타임레코드 : ', self.time_record)
         self.periodicTaskThread = Thread1(self, self.ser, self.dataCheckPoint, self.input_Value_signal, self.dataCheckPoint_signal, self.progress_signal, self.text_list, self.close_signal, self.record_flag, self.time_record, self.record_signal)
         self.periodicTaskThread.start()
 
-        # self.period_pbar_Timer = QTimer(self)
-        # self.period_pbar_Timer.timeout.connect()
-        # self.period_pbar_Timer.start(200)  
+        
+        # --------------Interval의 시간 초를 카운트 다운을 반복적으로 수행합니다.--------------
+        # 타이머설정
+        self.updateRemainingTime()
+        self.countDownTimer.start(1000) # 1초마다 반복, updateRemainingTime은 1초마다 카운트다운함.
+
+
+
+
+        #----------------------------------------------------------------------
+        
 
     def inputBtn_Push(self):
         self.ByteLength = 65
         #데이터 체크포인트 설정
         self.dataCheckPoint = False
+
+        # 버튼 초기화
+        self.recordStart_Btn.setEnabled(True)
+        
+
         # 입력된 주기
         str_interval = self.lineEdit_Interval.text().strip()
-        print(str_interval)
+        print("주기 : ", str_interval)
         if not str_interval:
             return
         interval = float(str_interval)
@@ -385,13 +425,17 @@ class WindowClass(QMainWindow, form_class):
             return
         
         self.period = interval * 1000 # ms 단위 이므로 1000울 곱합니다.
-
+       
+        
+        self.countDownTimer = QTimer(self)
+        self.countDownTimer.timeout.connect(self.updateRemainingTime)
 
         # 주기적인 작업을 시작합니다.
         self.startPeriodicTask()
         self.time_record = []
+        
         # 주기적으로 버튼을 클릭하는 것을 시뮬레이션하기 위해 QTimer를 사용합니다.
-        self.simulate_timer.start(int(self.period)) # 10000 period
+        self.simulate_timer.start(self.period)
     
     def Interval_Cancel_Btn_Push(self):
         if self.simulate_timer.isActive():
@@ -418,6 +462,7 @@ class WindowClass(QMainWindow, form_class):
     
     def recordStart_btn_push(self):
         # recordStart_btn 비활성화
+        print("기록 시작")
         self.recordStart_Btn.setEnabled(False)
         # 데이터를 읽는 스레드에 시간을 담으라고 Flag를 True 해준다. True는 담기 시작하라는 의미, False는 그만 담으라는 의미이다.
         self.record_flag = True
@@ -464,9 +509,9 @@ class WindowClass(QMainWindow, form_class):
             
         
     def updateTable(self):
-        print(len(self.received_data))
+        #print(len(self.received_data))
         main_table = getattr(self, 'df_table')
-        print(self.text_list)
+        #print(self.text_list)
         # Home Tab 부분의 테이블 실행
         if self.text_list == ['A', 'B', 'C', 'D', 'E']:
             self.updateHomeTab()
@@ -477,14 +522,14 @@ class WindowClass(QMainWindow, form_class):
 
 
     def updateHomeTab(self):
-    
+        print("호출됨")
         STX = self.received_data[0]
         return_cmd = self.received_data[1]
         rack_num = self.received_data[2]
         
         # 한 번만 시행됨.
         if self.dataCheckPoint == False:
-            print("datacheck")
+            #print("datacheck")
             
             self.dataCheckPoint = True
             self.received_data.pop(0)
@@ -500,8 +545,7 @@ class WindowClass(QMainWindow, form_class):
 
                 Vmodule_H = int(self.received_data[(i*12) + 1], 16)
                 Vmodule_L = int(self.received_data[(i*12) + 2], 16)
-                Voltage = "{:.3f}".format(round(float(Vmodule_H *256 + Vmodule_L) * 0.1, 3))
-                print(Voltage) 
+                Voltage = "{:.3f}".format(round(float(Vmodule_H *256 + Vmodule_L) * 0.1, 3)) 
                 temp_list.append(str(Voltage) + " " + "V")
                 
                 
@@ -515,21 +559,21 @@ class WindowClass(QMainWindow, form_class):
                 Vcell_max_L = int(self.received_data[(i*12) + 4], 16)
                 max_Cell = "{:.3f}".format(round(float(Vcell_max_H *256 + Vcell_max_L) * 0.001, 3))
                 temp_list.append(str(max_Cell) + " "+ "V")            
-                print(max_Cell)
+                
                 Vcell_min_H = int(self.received_data[(i*12) + 5], 16)
                 Vcell_min_L = int(self.received_data[(i*12) + 6], 16)
                 min_Cell = "{:.3f}".format(round(float(Vcell_min_H * 256 + Vcell_min_L) * 0.001, 3))
                 temp_list.append(str(min_Cell) + " " + "V")
-                print(min_Cell)
+                
                 Diff_V = "{:.3f}".format(round(abs(float(max_Cell) - float(min_Cell)), 3))
                 temp_list.append(str(Diff_V) + " " + "V")
-                print(Diff_V)
+                
                 max_T = int(self.received_data[(i*12) + 7], 16) - 50
                 min_T = int(self.received_data[(i*12) + 8], 16) - 50
                 temp_list.append(str(max_T) + " " + chr(176) + "C")
-                print(max_T)
+                
                 temp_list.append(str(min_T) + " " + chr(176) + "C")
-                print(min_T)
+                
                 diff_T = abs(max_T - min_T)
                 temp_list.append(str(diff_T) + " " + chr(176) + "C")
                 separated_LengthList_12.append(temp_list)
@@ -584,7 +628,7 @@ class WindowClass(QMainWindow, form_class):
 
 
             SW_Ver = round(int(self.received_data[9], 16) * 0.01, 2)
-            print(SW_Ver)
+            
 
 
             # Dianostic 부분 쉬프트 연산
@@ -597,14 +641,14 @@ class WindowClass(QMainWindow, form_class):
             # 쉬프트 연산을 시작
             shift_n = 0x01
             # 총 2byte길이, 비트로 따지면 2^16임
-            print(type(Dianostic))
+            
             for k in range(0, 16):
                 # Dianostic의 16비트와 16진수의 1을 비트 AND 시행
                 value = Dianostic & shift_n
                 Dianostic_list.append(value)
                 # 쉬프트 연산 이동
                 shift_n = (0x01 << (k+1))
-            print(Dianostic_list)
+            
 
         #입력 값이 'A'면, 모듈의 개수만큼의 행들의 값들이 채워진다.
 
@@ -669,31 +713,74 @@ class WindowClass(QMainWindow, form_class):
             self.dianostic_table.setItem(11, 1, item)
         # 다시 초기화
         self.received_data = []
+
+    def convertTableToList(self, table):
+        table_data = []
+        for row in range(table.rowCount()):
+            row_data = []
+            for column in range(table.columnCount()):
+                item = table.item(row, column)
+                if item is not None:
+                    row_data.append(item.text())
+                else:
+                    row_data.append(" ") # 빈 셀
+            table_data.append(row_data)
+        return table_data
     
     def toCsvBtn_Push(self):
-        default_file_name = "DataFrame.csv"
+        default_file_name = "DataFrame2.csv"
 
         # QFileDialog의 리턴 값이 총 2가지 있는데, 두 번째 _은 리턴하는 두번 째 값에 관심이 없어 _로 처리
         # 첫번째 값 파일 경로 지정
         file_path, _ = QFileDialog.getSaveFileName(self, "Save CSV File", default_file_name, "CSV Files (*.csv)")
+        print(self.dataTable_record)
 
         if file_path:
             try:
                 # Open the file in write mode
-                with open(file_path, 'w', newline='',  encoding='utf-8') as csv_file:
+                with open(file_path, 'w', newline='',  encoding='utf-16') as csv_file:
                     csv_writer = csv.writer(csv_file)
+                    print("데이터 길이", len(self.dataTable_record))
+                    print("날짜 길이", len(self.time_record))
+                    #### -------- DataFrame table 저장 코드 -------------------------------####
+                    for cnt in range(len(self.dataTable_record)):
+                        # 날짜 
+                        header_date = ['date']
+                        # 리스트 컨프리헨션으로 헤더 데이터 추출
+                        header_data =  header_date + [self.df_table.horizontalHeaderItem(i).text() for i in range(self.df_table.columnCount())]
+                        csv_writer.writerow(header_data)
 
-                    # 리스트 컨프리헨션으로 헤더 데이터 추출
-                    header_data = [self.df_table.horizontalHeaderItem(i).text() for i in range(self.df_table.columnCount())]
-                    csv_writer.writerow(header_data)
-
-                    # Write data
-                    for row in range(self.df_table.rowCount()):
-                        row_data = [self.df_table.item(row, col).text() for col in range(self.df_table.columnCount())]
-                        csv_writer.writerow(row_data)
-
+                        # dataTable_record에 있는 df를 꺼내옴
+                        stored_df = self.dataTable_record[cnt]
+                        # Write data
+                        for row in range(self.df_table.rowCount()):
+                            
+                            row_dataFramedata = [stored_df[row][i] for i in range(len(stored_df[row]))]
+                            row_date = [self.time_record[cnt]]
+                            completeTable = row_date + row_dataFramedata
+                            csv_writer.writerow(completeTable)
+                    #### -------- DataFrame table 저장 코드 -------------------------------####
+                    
+                    #### -------- system 저장 코드 -------------------------------####
+                    for cnt in range(len(self.systemTable_record)):
+                        stored_systemTable = self.systemTable_record[cnt]
+                        for row in range(self.SystemTable.rowCount()):
+                            row_data = [stored_systemTable[row][i] for i in range(len(stored_systemTable[row]))]
+                            csv_writer.writerow(row_data) 
+                    #### -------- system 저장 코드 -------------------------------####
+                    
+                    #### -------- diansotic 저장 코드 -------------------------------####
+                    for cnt in range(len(self.dianosticTable_record)):
+                        stored_dianosticTable = self.dianosticTable_record[cnt]
+                        for row in range(self.dianostic_table.rowCount()):
+                            row_data = [stored_dianosticTable[row][i] for i in range(len(stored_dianosticTable[row]))]
+                            csv_writer.writerow(row_data)
+                    #### -------- diansotic 저장 코드 -------------------------------####
+                                 
                 print(f"Table data saved to {file_path}")
-
+                current_time = datetime.now()
+                parser_current_time = current_time.strftime("%Y년%m월%d일_%H시%M분%S초")
+                self.csvFileSaveDateLabel.setText(f'CSV 저장 일시: {parser_current_time}')
             except Exception as e:
                 print(f"Error saving CSV file: {e}")
     
@@ -756,7 +843,7 @@ class WindowClass(QMainWindow, form_class):
             convertNum = 4    
         current_tableWidget = getattr(self, f'Module_table_1')
         
-        print(convertNum)
+        
         
         for row in range(0, len(Vcell_list)):
 
